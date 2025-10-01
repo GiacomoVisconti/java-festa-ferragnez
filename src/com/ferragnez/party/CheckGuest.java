b2b89704d982e2098cb01600024210ad649724f3
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-10-01 11:23:00 +0200

    checkguest logic with for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,7 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
 //         Nel programma occorre:
@@ -14,7 +16,36 @@ public class CheckGuest {
 // Lista invitati: 
 // Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
 
-        // ' Inizializ
+        // ? Inizializzo l'array di invitati
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 
+        //Prendo in input in nome dell'utente
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il tuo nome e il tuo cognome");
+        String userName = input.nextLine().replaceAll("\\s+", "").toLowerCase();
+
+        //Creo una variabile booleana per il controllo della presenza dello username
+        boolean isPresent = false;
+
+        //Per ogni elemento dell'array
+        for (int i = 0; i < guests.length; i++) {
+
+            String guest = guests[i].replaceAll("\\s+", "").toLowerCase();
+            // System.out.println(guest);
+            
+            if (guest.equals(userName)) {
+                isPresent = true;
+            } 
+        }
+
+        if (isPresent) {
+            
+            System.out.println("Sei stato invitato");
+        } else {
+                System.out.println("Mi dispiace ma non sei invitato!");
+
+        }
+
+        input.close();
     }
 }

aac8913584f59edb2311ed5e9e0c4dac086d7486
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-10-01 10:50:47 +0200

    create new package and class

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,20 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+//         Nel programma occorre:
+// - creare e inizializzare l’array contenente i nomi degli invitati
+// - chiedere all’utente come si chiama
+// - verificare che il nome sia presente nella lista
+// - lasciarlo entrare o rispedirlo cortesemente da dove è venuto :occhiali_da_sole: 
+
+// Bonus
+// se avete usato il ciclo for per cercare il nome nella lista nella consegna base, implementare lo stesso programma usando il ciclo while, e viceversa
+
+// Lista invitati: 
+// Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic
+
+        // ' Inizializ
+
+    }
+}
